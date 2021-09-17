df03ef8c8118575da16eb1152788206fbb8b86ec
Gustavo Ossio <gossioj@gmail.com>
2021-09-17 16:42:38 -0500

    third

--- file.js
+++ file.js
@@ -1,12 +1,6 @@
-const fs = require('fs');
+const { crearTabla } = require("./helpers/multiplicar");
+
 
 const value = 5;
-let salida = '';
-for (let index = 1; index < 11; index++) {
-    salida = `${value} x ${index} = ${value * index}`;
-}
 
-fs.writeFileSync(`table/tabla-${value}.txt`, salida, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-  });
+crearTabla(value).then(data => console.log(data));
